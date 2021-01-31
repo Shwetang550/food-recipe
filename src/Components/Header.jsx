1ba91ae58fd255f317e7f17a6f0fa010fcf0a583
Shwetang <shwetangsingh5@gmail.com>
2021-01-31 17:18:13 +0530

    Header search

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import FastfoodIcon from '@material-ui/icons/Fastfood';
+import SearchIcon from '@material-ui/icons/Search';
 
 const Header = () => {
     return ( 
         <div className="jumbotron">
             <FastfoodIcon className="foodIcon" />
             <h1 className="disply-1">Header</h1>
+
+            <div class="input-group w-25 mx-auto">
+                <input type="text" className="form-control" placeholder="Search your Recipe" />
+                <span className="input-group-text">
+                    <SearchIcon />
+                </span>
+            </div>
         </div>
     );
 }
